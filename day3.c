aa949ebc21f9a6d3e4bbc96a2fdf66afd78edcbe
Sharon Singh <sharonsingh120807@gmail.com>
2026-02-22 18:22:01 +0530

    Add files via upload

---
+++ day3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, k;
+    int comparisons = 0;
+    int found = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[100];
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter key to search: ");
+    scanf("%d", &k);
+
+    for (i = 0; i < n; i++)
+    {
+        comparisons++; // Count every comparison
+
+        if (arr[i] == k)
+        {
+            printf("Found at index %d\n", i);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found)
+    {
+        printf("Not Found\n");
+    }
+
+    printf("Comparisons = %d\n", comparisons);
+
+    return 0;
+}
