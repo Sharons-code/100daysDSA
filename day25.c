6421903d0f72aa2bb0b0de9f0dae946593ceeee6
Sharon Singh <sharonsingh120807@gmail.com>
2026-02-25 14:32:57 +0530

    Add files via upload

---
+++ day25.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure definition
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+// Function to create linked list
+struct Node *createList(int n)
+{
+    struct Node *head = NULL, *temp = NULL, *newNode;
+    int value;
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &value);
+
+        newNode = (struct Node *)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (head == NULL)
+        {
+            head = newNode;
+            temp = newNode;
+        }
+        else
+        {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    return head;
+}
+
+// Function to count occurrences
+int countOccurrences(struct Node *head, int key)
+{
+    int count = 0;
+    struct Node *temp = head;
+
+    while (temp != NULL)
+    {
+        if (temp->data == key)
+        {
+            count++;
+        }
+        temp = temp->next;
+    }
+
+    return count;
+}
+
+int main()
+{
+    int n, key;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    struct Node *head = createList(n);
+
+    printf("Enter key to count: ");
+    scanf("%d", &key);
+
+    int result = countOccurrences(head, key);
+
+    printf("Number of occurrences of %d = %d\n", key, result);
+
+    return 0;
+}
